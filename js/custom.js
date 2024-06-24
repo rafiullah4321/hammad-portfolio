ed4481b72a68e4df6e6f510b604e86780a834ccf
rafiullah4321 <ru7302712@gmail.com>
2024-06-24 18:17:55 +0500

    first commit

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+  $('.navbar-toggler').click(function() {
+    $('#ftco-nav').collapse('toggle');
+  });
+});
+
+
 $(document).ready(function(){
     var owl = $('.testimonials-slider');
 
@@ -83,45 +90,4 @@ $(document).ready(function () {
   });
 });
 
-
-    
-
-  $(document).ready(function () {
-    var owl = $('.showcase');
-
-    owl.owlCarousel({
-      items: 4, // Show 4 items per slide
-      loop: true,
-      margin: 50,
-      autoplay: false, // Enable autoplay
-      autoplayTimeout: 4000, // Autoplay interval in milliseconds (3 seconds)
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1, // Show 1 item per slide on mobile
-          nav: true
-        },
-        800: {
-          items: 2, // Show 2 items per slide on tablets
-          nav: false
-        },
-        992: {
-          items: 5, // Show 4 items per slide on laptops
-          nav: false,
-          loop: true
-        }
-      }
-    });
-
-    // Listen for the carousel changed event
-    owl.on('changed.showcase', function (event) {
-      // Check if it's the last slide
-      if (event.page.index === event.page.count - 1) {
-        // Wait for a moment and then jump to the first slide
-        setTimeout(function () {
-          owl.trigger('to.owl.carousel', 0);
-        }, 3000); // Adjust the delay as needed
-      }
-    });
-  });
     
